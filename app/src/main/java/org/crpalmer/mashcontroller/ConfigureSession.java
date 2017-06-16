3f8bd768bc7148edae271c4725c93b487bc083a1
Christopher R. Palmer <crpalmer@gmail.com>
2017-06-16 15:18:57 -0400

    Empty project

---
+++ app/src/main/java/org/crpalmer/mashcontroller/ConfigureSession.java
@@ -0,0 +1,13 @@
+package org.crpalmer.mashcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigureSession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configure_session);
+    }
+}
