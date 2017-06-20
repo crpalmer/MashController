6dee634b86a10e015b013086b92b480a652bfe61
Christopher R. Palmer <crpalmer@gmail.com>
2017-06-19 20:51:10 -0400

    Make the keyboard go away on "okay" and change the pump button

--- app/src/main/java/org/crpalmer/mashcontroller/MashStatus.java
+++ app/src/main/java/org/crpalmer/mashcontroller/MashStatus.java
@@ -1,9 +1,13 @@
 package org.crpalmer.mashcontroller;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -13,12 +17,24 @@ import android.widget.ToggleButton;
 public class MashStatus extends AppCompatActivity {
 
     private final BrewBoss brewBoss = new BrewBoss();
+    private View top;
     private DecimalInput targetTemperatureInput;
+    private BrewButton pumpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mash_status);
+
+        top = findViewById(R.id.mashStatusTop);
+        top.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focussed) {
+                if (focussed) {
+                    hideSoftKeyboard(top);
+                }
+            }
+        });
 
         targetTemperatureInput = new DecimalInput(R.id.targetTemperature, R.id.targetTempOkay, R.id.targetTempCancel) {
             @Override
@@ -33,30 +49,30 @@ public class MashStatus extends AppCompatActivity {
             }
         };
 
-        ToggleButton pumpOnButton = (ToggleButton) findViewById(R.id.pumpOnButton);
-        pumpOnButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        pumpButton = new BrewButton(R.id.pumpOnButton) {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                try {
-                    brewBoss.setPumpOn(b);
-                } catch (BrewBossConnectionException e) {
-                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-                }
+            public void changeState(boolean newState) throws BrewBossConnectionException {
+                setVisualState(newState);
+                brewBoss.setPumpOn(newState);
             }
-        });
+        };
+        pumpButton.setVisualState(brewBoss.isPumpOn());
+    }
+
+    private void hideSoftKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     private abstract class DecimalInput {
         private EditText editText;
         private ImageButton okayButton;
         private ImageButton cancelButton;
-        private View top;
 
         public DecimalInput(int editTextId, int okayButtonId, int cancelButtonId) {
             editText = (EditText) findViewById(editTextId);
             okayButton = (ImageButton) findViewById(okayButtonId);
             cancelButton = (ImageButton) findViewById(cancelButtonId);
-            top = findViewById(R.id.mashStatusTop);
 
             okayButton.setVisibility(View.INVISIBLE);
             cancelButton.setVisibility(View.INVISIBLE);
@@ -67,7 +83,8 @@ public class MashStatus extends AppCompatActivity {
                     if (! focussed) {
                         resetValue();
                     }
-                    setEditingMode(focussed);
+                    okayButton.setVisibility(focussed ? View.VISIBLE : View.INVISIBLE);
+                    cancelButton.setVisibility(focussed ? View.VISIBLE : View.INVISIBLE);
                 }
             });
 
@@ -92,16 +109,39 @@ public class MashStatus extends AppCompatActivity {
             });
         }
 
-        private void setEditingMode(boolean isEditing) {
-            okayButton.setVisibility(isEditing ? View.VISIBLE : View.INVISIBLE);
-            cancelButton.setVisibility(isEditing ? View.VISIBLE : View.INVISIBLE);
-        }
-
         private void resetValue() {
-             editText.setText(getCurrentValue());
+            editText.setText(getCurrentValue());
         }
 
         public abstract void onValueChanged(double value);
         public abstract String getCurrentValue();
+    }
+
+    private abstract class BrewButton implements View.OnClickListener {
+        private Button button;
+        private boolean currentState;
+
+        BrewButton(int buttonId) {
+            button = (Button) findViewById(buttonId);
+            button.setOnClickListener(this);
+        }
+
+        public void setVisualState(boolean enabled) {
+            button.setBackgroundColor(enabled ? Color.GREEN : Color.LTGRAY);
+            button.setText(enabled ? "ON" : "OFF");
+            currentState = enabled;
+        }
+
+        @Override
+        public void onClick(View view) {
+            try {
+                changeState(! currentState);
+            } catch (BrewBossConnectionException e) {
+                Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        public abstract void changeState(boolean newState) throws BrewBossConnectionException;
+
     }
 }
